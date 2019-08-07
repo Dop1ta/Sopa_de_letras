83b133a424265b84641825ceadb0184b5ac0cffe
apduska <50720951+apduska@users.noreply.github.com>
2019-08-07 00:04:12 -0400

    Update ejer3.c

--- ejer3.c
+++ ejer3.c
@@ -8,35 +8,50 @@
 
 void Aleatorio();
 void Datos();
-void casos();
 
 char M[n][n] = {0}; //matriz
+char Palabras[n][n]; //guarda las palabras
+int tam[n];//tamaño de las palabras
 int nu; //nxn
 int k; //cantidad de palabras
 float save[20]; //guarda las coordenadas de las palabras
-int contador=0;
+int contador1=0;
 int v2=0;
+int v3;
 
-struct Empire_State{ //alamacena las palabras (vector de estructura)
-    char palabras[20];
-};
-
-void horizontal( struct Empire_State V1);
-void vertical( struct Empire_State V1);
-void Diagonal_1( struct Empire_State V1);
-void Diagonal_2( struct Empire_State V1);
-
-void contador(struct Empire_State);   
+void contador();
+void horizontal();
+void vertical();
+void Diagonal_1();
+void Diagonal_2();
+ 
 
 int main(){
+    int numero;
     Datos(); //funcion que pide numeros
-
-    struct Empire_State V[k];
-
     printf("ingrese las palabras");
-    for( int i=0 ; i<k ; i++)
-       scanf("%s",V[i].palabras);
-
+    for( int i=0 ; i<k ; i++){
+        for( int j=0 ; j<k ; j++){
+            scanf("%s",&Palabras[i][j]);
+        }
+    }
+    contador();
+    for( v3=0 ;v3<k ; v3++){   
+        srand(time(NULL));
+        numero = rand() % (3+1);
+        if( numero==0 ){ //horizontal
+            horizontal();
+        }
+        if( numero==1 ){ //vertical
+            vertical();
+        }
+        if( numero==2 ){ //primera diagonal
+            Diagonal_1();
+        }
+        if( numero==3 ){//segunda diagonal
+            Diagonal_2();
+        }
+    }
     system("cls");
 
 
@@ -55,29 +70,22 @@ void Datos(){// pide los valores de las variables globales
 void Aleatorio(){ //se encarga de regenar la matriz hasta el nxn = nu, ingresado por el usuario
     srand(time(NULL));
     int i,j;
-    for( i=0 ; i<nu ; i++)
-        for( j=0 ; j<nu ; j++ )
+    for( i=0 ; i<nu ; i++){
+        for( j=0 ; j<nu ; j++ ){
             M[i][j]=  'a' + rand() % (('z' - 'a') + 1);
-
-}
-void casos(){
-    int numero;
-    srand(time(NULL));
-    numero = rand() % (3+1);
-    if( numero==0 ){ //horizontal
-        horizontal();
-    }
-    if( numero==1 ){ //vertical
-        vertical();
-    }
-    if( numero==2 ){ //primera diagonal
-        Diagonal_1();
-    }
-    if( numero==3 ){//segunda diagonal
-        Diagonal_2();
+        }
     }
 }
-void horizontal( struct Empire_State V1 ){
+void contador(){
+    int i,p;
+    for( i=0 ; i<k ; i++ ){
+        for( p=0 ; Palabras[i][p]!='\0' ; p++ ){
+        }
+        tam[i]=p-1;
+    }
+}
+void horizontal(){
+    int ss;
     v2++; //struct
     int con1=0; //cuenta las palabras hacia adelante
     int i,j; //indices del for
@@ -91,34 +99,38 @@ void horizontal( struct Empire_State V1 ){
             y++;
             con1++;
         }
-            if( con1>=(strlen(V1[v2]->palabras)-1) ){
+            if( con1>=tam[v2] ){
+                ss=0;
                 for( int i=(y-con1) ; i<nu ; i++){
                     if(M[x][i]==0){
-                        M[x][i]=V1[v2]->palabras;
+                        M[x][i]=Palabras[v3][ss];
                     }
+                    ss++;
                 }
                 if( i<nu || j<nu ){
-                    contador++;
-                    save[contador] = y/10;
-                    save[contador] += x;
+                    contador1++;
+                    save[contador1] = y/10;
+                    save[contador1] += x;
                 }
             }  
-        if( y>=(strlen(V1[v2]->palabras)-1) ){
+        if( y>=tam[v2] ){
+            ss=0;
             for( int j=(y-con1) ; j>-1 ; j--){
-                if(M[x][j]==0 || M[x][j]==V1[v2]->palabras){
-                    M[x][j]=V1[v2]->palabras;
+                if(M[x][j]==0 ){
+                    M[x][j]=Palabras[v3][ss];
                 }
+                ss++;
             }
             if( i<nu || j<nu ){
-                contador++;
-                save[contador] = y/10;
-                save[contador] += x;
+                contador1++;
+                save[contador1] = y/10;
+                save[contador1] += x;
             }
         }
     }
 }
-
-void vertical( struct Empire_State V1 ){
+void vertical(){
+    int ss; 
     v2++;
     int con2;
     int i,j; //indices del for
@@ -132,39 +144,84 @@ void vertical( struct Empire_State V1 ){
             x++;
             con2++;
         }
-            if( con2>=(strlen(V1[v2]->palabras)-1) ){
+            if( con2>=tam[v2] ){
                 for( int i=(x-con2) ; i<nu ; i++){
                     if(M[i][y]==0){
-                        M[i][y]=V1[v2]->palabras;
+                        M[i][y]=Palabras[v3][ss];
                     }
+                    ss++;
                 }
                 if( i<nu || j<nu ){
-                    contador++;
-                    save[contador] = y/10;
-                    save[contador] += x;
+                    contador1++;
+                    save[contador1] = y/10;
+                    save[contador1] += x;
                 }
             }  
-        if( x>=(strlen(V1[v2]->palabras)-1) ){
+        if( x>=tam[v2] ){
             for( int j=x ; j>-1 ; j--){
-                if(M[j][y]==0 || M[x][j]==V1[v2]->palabras){
-                    M[j][y]=V1[v2]->palabras;
+                if(M[j][y]==0 ){
+                    M[j][y]=Palabras[v3][ss];
                 }
+                ss++;
             }
             if( i<nu || j<nu ){
-                contador++;
-                save[contador] = y/10;
-                save[contador] += x;
+                contador1++;
+                save[contador1] = y/10;
+                save[contador1] += x;
             }
         }
     }
 }
-void Diagonal_1( struct Empire_State V1 ){
+void Diagonal_1(){
+    int ss;
     v2++;
     int con1=0;
     int i,j; //indices del for
     int x,y; // coordenadas de la palabra 
     srand(time(NULL));  
-    while( i<nu || j<nu ){
+    while( 1 ){
+        x = rand() % (nu+1);   //tamaño x de nu que es de la matriz 
+        y = rand() % (nu+1);   //tamaño y de nu que es de la matriz
+        
+        while( y<nu ){
+            y++;
+            x++;
+            con1++;
+        }
+        if( con1>=tam[v2] ){ //diagonal izquierda normal
+            for( i=(y-con1),j=(x-con1) ; i<con1 ; i++,j++){
+                if(M[i][j]==0){
+                    M[i][j]==Palabras[v3][ss];
+                }
+                ss++;
+            }
+            contador1++;
+            save[contador1] = y/10;
+            save[contador1] += x;
+            break;
+        }
+        if( (y-con1)>=tam[v2] ){ //diagonal izquierda alreves
+            for( i=(y-con1),j=(x-con1) ; j>=0 ; i--,j--){
+                if(M[i][j]==0){
+                    M[i][j]==Palabras[v3][ss];
+                }
+                ss++;
+            }
+            contador1++;
+            save[contador1] = y/10;
+            save[contador1] += x;
+            break;            
+        }
+    }
+}
+void Diagonal_2(){
+    int ss;
+    v2++;
+    int con1=0;
+    int i,j; //indices del for
+    int x,y; // coordenadas de la palabra 
+    srand(time(NULL));  
+    while( 1 ){
         x = rand() % (nu+1);   //tamaño x de nu que es de la matriz 
         y = rand() % (nu+1);   //tamaño y de nu que es de la matriz
         
@@ -173,29 +230,29 @@ void Diagonal_1( struct Empire_State V1 ){
             x++;
             con1++;
         }
-        if( con1>=(strlen(V1[v2]->palabras)-1) ){ //diagonal izquierda normal
-            for( i=(y-con1),j=(x-con1) ; i<con1 ; i++,j++){
+        if( con1>=tam[v2] ){ //diagonal izquierda normal
+            for( i=(y-con1),j=(x-con1) ; i<con1 ; i--,j++){
                 if(M[i][j]==0){
-                    M[i][j]==V1[v2]->palabras;
+                    M[i][j]==Palabras[v3][ss];
                 }
+                ss++;
             }
-            if( i<nu || j<nu ){
-                contador++;
-                save[contador] = y/10;
-                save[contador] += x;
-            }
+            contador1++;
+            save[contador1] = y/10;
+            save[contador1] += x;
+            break;
         }
-        if( (y-con1)>=(strlen(V1[v2]->palabras)-1) ){ //diagonal izquierda alreves
-            for( i=(y-con1),j=(x-con1) ; i>=0 ; i--,j--){
+        if( (y-con1)>=tam[v2] ){ //diagonal izquierda alreves
+            for( i=(y-con1),j=(x-con1) ; i<nu ; i++,j--){
                 if(M[i][j]==0){
-                    M[i][j]==V1[v2]->palabras;
+                    M[i][j]==Palabras[v3][ss];
                 }
+                ss++;
             }
-            if( i<nu || j<nu ){
-                contador++;
-                save[contador] = y/10;
-                save[contador] += x;
-            }            
+            contador1++;
+            save[contador1] = y/10;
+            save[contador1] += x;            
+            break;
         }
     }
 }
